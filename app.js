702c39e22d0272352312de6b4bfdcc8cf211a458
GenaVinokurov <genabrest97@gmail.com>
2021-11-26 15:29:48 +0300

    refactor: formatting styles according rule 3.4

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var taskInput = document.getElementById("new-task");//Add a new task.
 var addButton = document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder = document.getElementById("wait");//ul of #incompleteTasks
 var completedTasksHolder = document.getElementById("completed");//completed-tasks
-
+console.log(incompleteTaskHolder)
 
 //New task list item
 var createNewTaskElement = function (taskString) {
@@ -85,7 +85,7 @@ var editTask = function () {
     var editInput = listItem.querySelector('input[type=text]');
     var label = listItem.querySelector("label");
     var editBtn = listItem.querySelector(".edit");
-    var containsClass = listItem.classList.contains("editMode");
+    var containsClass = listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
     if (containsClass) {
 
@@ -99,7 +99,7 @@ var editTask = function () {
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    listItem.classList.toggle("edit-mode");
 };
 
 
